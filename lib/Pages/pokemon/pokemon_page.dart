fa9307bf205d70a4da557e3d434e035163af3685
israelsilva282 <israelcostas@hotmail.com>
2023-06-10 08:54:52 -0300

    feat: add pokemon view page (#1)

---
+++ lib/Pages/pokemon/pokemon_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Pokemon extends StatelessWidget {
+  const Pokemon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("PokeHOME"),
+      ),
+    );
+  }
+}
