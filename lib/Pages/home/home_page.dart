ef2a9faae30fbd345dc2a694129b3dd7b4964479
isazvdd <isadora.azevedo.700@ufrn.edu.br>
2023-06-12 14:01:59 -0300

    feat: colocando a lista de Pokemons na home (#2)

--- lib/Pages/home/home_page.dart
+++ lib/Pages/home/home_page.dart
@@ -1,12 +1,11 @@
 import 'dart:convert';
 
-import 'package:dartproject/Components/appBar/appbar_component.dart';
 import 'package:dartproject/Components/bottomNavBar/bottomnavbar_component.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class DataService {
-  final ValueNotifier<List> tableStateNotifier = ValueNotifier([]);
+  final ValueNotifier<List<dynamic>> tableStateNotifier = ValueNotifier([]);
   final ValueNotifier<List<String>> propertyNamesNotifier = ValueNotifier([]);
 
   DataService() {
@@ -15,42 +14,66 @@ class DataService {
 
   Future<void> loadPokemons({int limit = 10}) async {
     var pokeUri = Uri(
-        scheme: 'https',
-        host: 'pokeapi.co',
-        path: 'api/v2/pokemon/',
-        queryParameters: {
-          'limit': limit.toString(),
-          'offset': '0',
-        });
+      scheme: 'https',
+      host: 'pokeapi.co',
+      path: 'api/v2/pokemon/',
+      queryParameters: {
+        'limit': limit.toString(),
+        'offset': '0',
+      },
+    );
 
-    var jsonString = await http.read(pokeUri);
+    var response = await http.get(pokeUri);
 
-    var pokeJson = jsonDecode(jsonString);
+    if (response.statusCode == 200) {
+      var pokeJson = jsonDecode(response.body);
 
-    var pokemonList = pokeJson['results'];
+      var pokemonList = pokeJson['results'];
 
-    var pokemons = <dynamic>[];
+      var pokemons = <dynamic>[];
 
-    for (var pokemon in pokemonList) {
-      var pokemonUri = Uri.parse(pokemon['url']);
-      var pokemonJsonString = await http.read(pokemonUri);
-      var pokemonJson = jsonDecode(pokemonJsonString);
-      pokemons.add(pokemonJson);
+      for (var pokemon in pokemonList) {
+        var pokemonUri = Uri.parse(pokemon['url']);
+        var pokemonResponse = await http.get(pokemonUri);
+        if (pokemonResponse.statusCode == 200) {
+          var pokemonJson = jsonDecode(pokemonResponse.body);
+          pokemons.add(pokemonJson);
+        }
+      }
+
+      tableStateNotifier.value = pokemons;
+    } else {
+      print(
+          'Falha ao carregar os pokémons. Código de status: ${response.statusCode}');
     }
-
-    tableStateNotifier.value = pokemons;
   }
 }
 
 final dataService = DataService();
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: MyAppBar(title: "PokeHome"),
+    return Scaffold(
+      appBar: AppBar(title: const Text("PokeHome")),
+      body: ValueListenableBuilder<List<dynamic>>(
+        valueListenable: dataService.tableStateNotifier,
+        builder: (context, pokemons, child) {
+          return ListView.builder(
+            itemCount: pokemons.length,
+            itemBuilder: (context, index) {
+              var pokemon = pokemons[index];
+              return ListTile(
+                title: Text(pokemon['name']),
+                subtitle: Text('ID: ${pokemon['id']}'),
+                leading: Image.network(pokemon['sprites']['front_default']),
+              );
+            },
+          );
+        },
+      ),
       bottomNavigationBar: MyBottomNavbar(),
     );
   }
