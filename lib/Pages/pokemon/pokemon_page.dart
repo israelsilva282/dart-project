f494cf5d0f64a3e9e22e3fe8af9f9d2f40200ed3
isazvdd <isadora.azevedo.700@ufrn.edu.br>
2023-06-13 14:20:31 -0300

    feat: adicionando tabela de informações do pokemon (#5)

--- lib/Pages/pokemon/pokemon_page.dart
+++ lib/Pages/pokemon/pokemon_page.dart
@@ -2,6 +2,27 @@ import 'package:dartproject/Components/appBar/appbar_component.dart';
 import 'package:flutter/material.dart';
 
 class Pokemon extends StatelessWidget {
+  dynamic _newRow(title, description) {
+    return TableRow(children: [
+      Container(
+          alignment: Alignment.centerRight,
+          padding: const EdgeInsets.all(10),
+          child: Text(
+            title.toString().toUpperCase(),
+            style: const TextStyle(
+              fontSize: 10,
+              fontWeight: FontWeight.bold,
+            ),
+          )),
+      Container(
+          padding: const EdgeInsets.all(10),
+          child: Text(
+            description.toString().toUpperCase(),
+            style: const TextStyle(fontSize: 10),
+          )),
+    ]);
+  }
+
   final dynamic pokemon;
   const Pokemon({super.key, required this.pokemon});
 
@@ -25,7 +46,27 @@ class Pokemon extends StatelessWidget {
                   width: double.infinity,
                   height: 250,
                 )),
-            Text(pokemon['id'].toString())
+            Table(
+              children: [
+                _newRow('id', pokemon['id']),
+                _newRow(
+                    'Habilidade',
+                    pokemon['abilities'][0]['ability']['name'] +
+                        " " +
+                        pokemon['abilities'][1]['ability']['name']),
+                _newRow(
+                    'Tipo',
+                    pokemon['types'][0]['type']['name'] +
+                        " " +
+                        pokemon['types'][1]['type']['name']),
+                _newRow('hp', pokemon['stats'][0]['base_stat']),
+                _newRow('ataque', pokemon['stats'][1]['base_stat']),
+                _newRow('defesa', pokemon['stats'][2]['base_stat']),
+                _newRow('ataque especial', pokemon['stats'][3]['base_stat']),
+                _newRow('defesa especial', pokemon['stats'][4]['base_stat']),
+                _newRow('velocidade', pokemon['stats'][5]['base_stat']),
+              ],
+            )
           ],
         )));
   }
