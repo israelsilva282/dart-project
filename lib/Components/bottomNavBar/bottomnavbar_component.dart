39fdb086c1f8aa899b2ad83babc5c207a7b6f05c
israelsilva282 <israelcostas@hotmail.com>
2023-06-19 13:43:51 -0300

    feat: iniciado template de Search

--- lib/Components/bottomNavBar/bottomnavbar_component.dart
+++ lib/Components/bottomNavBar/bottomnavbar_component.dart
@@ -3,16 +3,11 @@ import 'package:flutter/material.dart';
 class MyBottomNavbar extends StatelessWidget {
   const MyBottomNavbar({super.key});
 
-  void _navigate(index, context) {
-    if (index == 0) Navigator.pushNamed(context, '/');
-    if (index == 1) Navigator.pushNamed(context, '/search');
-  }
-
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
       onPressed: () {
-        Navigator.pushNamed(context, '/');
+        Navigator.pushNamed(context, '/search');
       },
       child: const Icon(Icons.search),
     );
--- lib/Pages/search/search_page.dart
+++ lib/Pages/search/search_page.dart
@@ -1,13 +1,32 @@
-import 'package:dartproject/Components/appBar/appbar_component.dart';
 import 'package:flutter/material.dart';
+import 'package:states_rebuilder/scr/state_management/rm.dart';
 
-class Search extends StatelessWidget {
+class Search extends ReactiveStatelessWidget {
   const Search({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: MyAppBar(title: "Search"),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colors.black),
+          elevation: 0,
+          backgroundColor: Colors.white,
+        ),
+        body: const Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            FractionallySizedBox(
+              widthFactor: 0.8,
+              child: TextField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                    hintText: "Digite o nome do Pokémon...",
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black))),
+              ),
+            ),
+            Text("Test")
+          ],
+        ));
   }
 }
